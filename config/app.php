5ed8741b6c7c4680bfe0e18d250bc8a2fb77d70e
faizunroyalbengalai <faizun.royalbengalai@gmail.com>
2026-05-15 22:59:19 +0600

    Update via DevOps Agent

---
+++ config/app.php
@@ -0,0 +1,2 @@
+<?php
+return ['name' => env('APP_NAME', 'Laravel'), 'env' => env('APP_ENV', 'production'), 'debug' => env('APP_DEBUG', false), 'url' => env('APP_URL', 'http://localhost'), 'timezone' => 'UTC', 'locale' => 'en', 'fallback_locale' => 'en', 'faker_locale' => 'en_US', 'cipher' => 'AES-256-CBC', 'key' => env('APP_KEY'), 'maintenance' => ['driver' => 'file']];
